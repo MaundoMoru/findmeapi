211402a30a34cd3ee3adc7e6196486734e232009
Joseph Kamande <joseph.kamande@ronforddigital.com>
2023-02-20 10:50:58 +0300

    changes

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize  = require('sequelize');
 
-module.exports = new Sequelize('findme', 'postgres', 'Joseph12', {
-    host: 'localhost',
+module.exports = new Sequelize('findme', 'findme_user', '8XdiYNt4bg6p8jzSsKTMgWRVRwX9ssed', {
+    host: 'dpg-cfpi5qqrrk0fd9to968g-a',
     dialect: 'postgres',
     operatorsAliases: false,
 
