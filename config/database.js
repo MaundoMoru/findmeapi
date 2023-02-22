a39c3b9e69c7529735ab7c3979f98895b3c2f592
Joseph Kamande <joseph.kamande@ronforddigital.com>
2023-02-22 11:04:36 +0300

    chages

--- config/database.js
+++ config/database.js
@@ -1,20 +1,7 @@
 const Sequelize  = require('sequelize');
 
-// module.exports = new Sequelize('findme_xvrq', 'findme_xvrq_user', 'qPxhxeAN8UgNwjEjmHXeGCqPSnPYX4v6', {
-//     host: 'dpg-cfq8n01gp3jo0b6a24u0-a',
-//     dialect: 'postgres',
-//     operatorsAliases: false,
-
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     },
-//   });
-
-  module.exports = new Sequelize('findme', 'postgres', 'Joseph12', {
-    host: 'localhost',
+module.exports = new Sequelize('findme_xvrq', 'findme_xvrq_user', 'qPxhxeAN8UgNwjEjmHXeGCqPSnPYX4v6', {
+    host: 'dpg-cfq8n01gp3jo0b6a24u0-a',
     dialect: 'postgres',
     operatorsAliases: false,
 
@@ -25,3 +12,16 @@ const Sequelize  = require('sequelize');
       idle: 10000
     },
   });
+
+  // module.exports = new Sequelize('findme', 'postgres', 'Joseph12', {
+  //   host: 'localhost',
+  //   dialect: 'postgres',
+  //   operatorsAliases: false,
+
+  //   pool: {
+  //     max: 5,
+  //     min: 0,
+  //     acquire: 30000,
+  //     idle: 10000
+  //   },
+  // });
