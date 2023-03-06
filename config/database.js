edba794c47ab6b42065e4f6630e2d37e8e7eabb6
Joseph Kamande <joseph.kamande@ronforddigital.com>
2023-03-06 08:31:14 +0300

    commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize  = require('sequelize');
 
-module.exports = new Sequelize('findme_xvrq', 'findme_xvrq_user', 'qPxhxeAN8UgNwjEjmHXeGCqPSnPYX4v6', {
-    host: 'dpg-cfq8n01gp3jo0b6a24u0-a',
+module.exports = new Sequelize('findme_ae6d', 'findme_ae6d_user', 'Y5hnFD3emxrkwGGNqzvZygOX8HlMdvou', {
+    host: 'dpg-cg2nkd02qv24hdltdd80-a',
     dialect: 'postgres',
     operatorsAliases: false,
 
